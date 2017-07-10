22588b360790c4f5fad4c7ed71596425571dd09d
Prasad <programworks11@users.noreply.github.com>
2017-07-10 17:51:58 +0530

    Updated to include some padding and margin between components

--- src/components/app.js
+++ src/components/app.js
@@ -6,9 +6,15 @@ export default class App extends Component
   render()
   {
     return (<div>
+              <div style={{float:"left", border:"1px solid black", padding:"20px 20px 20px 20px", margin:"10px 10px 10px 10px"}} >
                 <AppContainer />
+              </div>
+              <div style={{float:"left", border:"1px solid black", padding:"20px 20px 20px 20px", margin:"10px 10px 10px 10px"}} >
                 <AppContainer />
+              </div>
+              <div style={{float:"left", border:"1px solid black", padding:"20px 20px 20px 20px", margin:"10px 10px 10px 10px"}} >
                 <AppContainer />
+              </div>
             </div>);
   }
 }
